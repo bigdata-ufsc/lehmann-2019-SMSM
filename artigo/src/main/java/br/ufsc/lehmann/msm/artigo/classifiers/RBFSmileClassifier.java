dbb3e64c9567f84b09b1927eb099344526e4aa43
uuario <uuario@150.162.58.174>
2017-06-30 10:19:02 -0300

    Implementado pesquisa através de RBF

---
+++ artigo/src/main/java/br/ufsc/lehmann/msm/artigo/classifiers/RBFSmileClassifier.java
@@ -0,0 +1,24 @@
+package br.ufsc.lehmann.msm.artigo.classifiers;
+
+import java.util.List;
+
+import br.ufsc.core.trajectory.SemanticTrajectory;
+import smile.classification.RBFNetwork;
+
+public class RBFSmileClassifier<Label> implements IClassifier<Label> {
+
+	private RBFNetwork<SemanticTrajectory> nn;
+	private List<Label> uniqueLabels;
+
+	public RBFSmileClassifier(RBFNetwork<SemanticTrajectory> knn, List<Label> uniqueLabels) {
+		nn = knn;
+		this.uniqueLabels = uniqueLabels;
+	}
+
+	@Override
+	public Label classify(SemanticTrajectory traj) {
+		int prediction = nn.predict(traj);
+		return uniqueLabels.get(prediction);
+	}
+
+}
