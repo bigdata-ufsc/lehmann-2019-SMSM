aa5c0b49cc5a7ca2601393b67372c3254930de86
uuario <uuario@150.162.58.174>
2017-08-16 12:53:09 -0300

    Nova base, novo problema

---
+++ artigo/src/main/java/br/ufsc/lehmann/msm/artigo/problems/SergipeTracksDataReader.java
@@ -0,0 +1,186 @@
+package br.ufsc.lehmann.msm.artigo.problems;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
+
+import com.google.common.collect.Multimap;
+import com.google.common.collect.MultimapBuilder;
+
+import br.ufsc.core.trajectory.Semantic;
+import br.ufsc.core.trajectory.SemanticTrajectory;
+import br.ufsc.core.trajectory.StopSemantic;
+import br.ufsc.core.trajectory.TPoint;
+import br.ufsc.core.trajectory.TemporalDuration;
+import br.ufsc.core.trajectory.semantic.Move;
+import br.ufsc.core.trajectory.semantic.Stop;
+import br.ufsc.db.source.DataRetriever;
+import br.ufsc.db.source.DataSource;
+import br.ufsc.db.source.DataSourceType;
+import br.ufsc.lehmann.MoveEllipsesSemantic;
+import br.ufsc.lehmann.MovePointsSemantic;
+import br.ufsc.lehmann.MoveSemantic;
+import br.ufsc.lehmann.MoveSemantic.Fields;
+import br.ufsc.lehmann.stopandmove.LatLongDistanceFunction;
+
+public class SergipeTracksDataReader {
+	
+	public static final BasicSemantic<Double> DISTANCE = new BasicSemantic<>(3);
+	public static final BasicSemantic<Integer> CAR_OR_BUS = new BasicSemantic<>(4);
+	public static final BasicSemantic<Double> AVERAGE_SPEED = new BasicSemantic<>(5);
+	public static final BasicSemantic<Integer> RATING = new BasicSemantic<>(6);
+	public static final BasicSemantic<String> LINHA = new BasicSemantic<>(7);
+	public static final BasicSemantic<Integer> RATING_BUS = new BasicSemantic<>(8);
+	public static final BasicSemantic<Integer> RATING_WEATHER = new BasicSemantic<>(9);
+	public static final StopSemantic STOP_SEMANTIC = new StopSemantic(10, new LatLongDistanceFunction());
+	public static final MoveSemantic MOVE_ANGLE_SEMANTIC = new MoveSemantic(11, Fields.ANGLE);
+	public static final MoveSemantic MOVE_DISTANCE_SEMANTIC = new MoveSemantic(11, Fields.DISTANCE);
+	public static final MovePointsSemantic MOVE_POINTS_SEMANTIC = new MovePointsSemantic(11, new LatLongDistanceFunction(), 10);
+	public static final MoveEllipsesSemantic MOVE_ELLIPSES_SEMANTIC = new MoveEllipsesSemantic(11);
+
+	public List<SemanticTrajectory> read() throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
+		DataSource source = new DataSource("postgres", "postgres", "localhost", 5432, "postgis", DataSourceType.PGSQL, "sergipe.tracks", null, null);
+		DataRetriever retriever = source.getRetriever();
+		System.out.println("Executing SQL...");
+		Connection conn = retriever.getConnection();
+		conn.setAutoCommit(false);
+		Statement st = conn.createStatement();
+		st.setFetchSize(1000);
+
+		ResultSet stopsData = st.executeQuery(
+				"SELECT stop_id, start_lat, start_lon, begin, end_lat, end_lon, length, centroid_lat, " + //
+						"centroid_lon, start_time, end_time " + //
+						"FROM stops_moves.sergipe_tracks_stop");
+		Map<Integer, Stop> stops = new HashMap<>();
+		while (stopsData.next()) {
+			int stopId = stopsData.getInt("stop_id");
+			Stop stop = stops.get(stopId);
+			if (stop == null) {
+				stop = new Stop(stopId, null, //
+						stopsData.getTimestamp("start_time").getTime(), //
+						stopsData.getTimestamp("end_time").getTime(), //
+						new TPoint(stopsData.getDouble("start_lat"), stopsData.getDouble("start_lon")), //
+						stopsData.getInt("begin"), //
+						new TPoint(stopsData.getDouble("end_lat"), stopsData.getDouble("end_lon")), //
+						stopsData.getInt("length"), //
+						new TPoint(stopsData.getDouble("centroid_lat"), stopsData.getDouble("centroid_lon"))//
+				);
+				stops.put(stopId, stop);
+			}
+		}
+		Map<Integer, Move> moves = new HashMap<>();
+		ResultSet movesData = st.executeQuery(
+				"SELECT move_id, start_time, start_stop_id, begin, end_time, end_stop_id, length, angle " + //
+						"FROM stops_moves.sergipe_tracks_move");
+		while(movesData.next()) {
+			int moveId = movesData.getInt("move_id");
+				Move move = moves.get(moveId);
+			if (move == null) {
+				int startStopId = movesData.getInt("start_stop_id");
+				if (movesData.wasNull()) {
+					startStopId = -1;
+				}
+				int endStopId = movesData.getInt("end_stop_id");
+				if (movesData.wasNull()) {
+					endStopId = -1;
+				}
+				move = new Move(moveId, //
+						stops.get(startStopId), //
+						stops.get(endStopId), //
+						movesData.getTimestamp("start_time").getTime(), //
+						movesData.getTimestamp("end_time").getTime(), //
+						movesData.getInt("begin"), //
+						movesData.getInt("length"), //
+						null,
+						movesData.getDouble("angle"));
+				moves.put(moveId, move);
+			}
+		}
+		String sql = "select t.id as tid, t.id_android, t.speed as average_speed, t.distance as traveled_distance, t.rating as rating, "//
+				+ "t.rating_bus as rating_bus, t.rating_weather as rating_weather, t.car_or_bus as car_or_bus, t.linha as linha, "//
+				+ "tp.semantic_stop_id as semantic_stop_id, tp.semantic_move_id as semantic_move_id, "//
+		/**/+ "tp.id as gid, tp.latitude as latitude, tp.longitude as longitude, tp.\"time\" as \"time\" "//
+		+ "from sergipe.tracks t inner join sergipe.trackpoints tp on t.id = tp.track_id "//
+		+ "order by t.id, t.\"time\"";//
+		PreparedStatement preparedStatement = conn.prepareStatement(sql);
+		ResultSet data = preparedStatement.executeQuery();
+		Multimap<Integer, SergipeTracksRecord> records = MultimapBuilder.hashKeys().linkedListValues().build();
+		System.out.println("Fetching...");
+		while(data.next()) {
+			Integer stop = data.getInt("semantic_stop_id");
+			if(data.wasNull()) {
+				stop = null;
+			}
+			Integer move = data.getInt("semantic_move_id");
+			if(data.wasNull()) {
+				move = null;
+			}
+			SergipeTracksRecord record = new SergipeTracksRecord(
+				data.getInt("gid"),
+				data.getTimestamp("time"),
+				data.getInt("tid"),
+				data.getDouble("average_speed"),
+				data.getDouble("traveled_distance"),
+				data.getDouble("longitude"),
+				data.getDouble("latitude"),
+				data.getInt("rating"),
+				data.getInt("rating_bus"),
+				data.getInt("rating_weather"),
+				data.getInt("car_or_bus"),
+				data.getString("linha"),
+				stop,
+				move
+			);
+			records.put(record.getTid(), record);
+		}
+		st.close();
+		System.out.printf("Loaded %d GPS points from database\n", records.size());
+		System.out.printf("Loaded %d trajectories from database\n", records.keySet().size());
+		List<SemanticTrajectory> ret = new ArrayList<>();
+		Set<Integer> keys = records.keySet();
+		DescriptiveStatistics stats = new DescriptiveStatistics();
+		for (Integer trajId : keys) {
+			SemanticTrajectory s = new SemanticTrajectory(trajId, 12);
+			Collection<SergipeTracksRecord> collection = records.get(trajId);
+			int i = 0;
+			for (SergipeTracksRecord record : collection) {
+				s.addData(i, Semantic.GID, record.getGid());
+				TPoint point = new TPoint(record.getLatitude(), record.getLongitude());
+				s.addData(i, Semantic.GEOGRAPHIC, point);
+				s.addData(i, Semantic.TEMPORAL, new TemporalDuration(Instant.ofEpochMilli(record.getTime().getTime()), Instant.ofEpochMilli(record.getTime().getTime())));
+				s.addData(i, AVERAGE_SPEED, record.getAverageSpeed());
+				s.addData(i, CAR_OR_BUS, record.getCar_or_bus());
+				s.addData(i, LINHA, record.getLinha());
+				s.addData(i, RATING, record.getRating());
+				s.addData(i, RATING_BUS, record.getRating_bus());
+				s.addData(i, RATING_WEATHER, record.getRating_weather());
+				s.addData(i, DISTANCE, record.getTraveledDistance());
+				if(record.getSemanticStop() != null) {
+					Stop stop = stops.get(record.getSemanticStop());
+					s.addData(i, STOP_SEMANTIC, stop);
+				}
+				if(record.getSemanticMoveId() != null) {
+					Move move = moves.get(record.getSemanticMoveId());
+					move.addPoint(point);
+					s.addData(i, MOVE_ANGLE_SEMANTIC, move);
+				}
+				i++;
+			}
+			stats.addValue(s.length());
+			ret.add(s);
+		}
+		System.out.printf("Semantic Trajectories statistics: mean - %.2f, min - %.2f, max - %.2f, sd - %.2f\n", stats.getMean(), stats.getMin(), stats.getMax(), stats.getStandardDeviation());
+		return ret;
+	}
+}
