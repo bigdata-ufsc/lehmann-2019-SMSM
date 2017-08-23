0d43aa5081214481514da6353b58f39d3b30199b
uuario <uuario@150.162.58.174>
2017-08-23 14:44:35 -0300

    Reverse geocoding em mais bases

---
+++ artigo/src/test/java/br/ufsc/lehmann/geocode/reverse/SanFranciscoTaxiFillDupesReverseGeocoding.java
@@ -0,0 +1,59 @@
+package br.ufsc.lehmann.geocode.reverse;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Map;
+
+import br.ufsc.db.source.DataRetriever;
+import br.ufsc.db.source.DataSource;
+import br.ufsc.db.source.DataSourceType;
+
+public class SanFranciscoTaxiFillDupesReverseGeocoding {
+
+	public static void main(String[] args) throws SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException {
+		DataSource source = new DataSource("postgres", "postgres", "localhost", 5432, "postgis", DataSourceType.PGSQL, "taxi.sanfrancisco_taxicab_crawdad", null,
+				null);
+		DataRetriever retriever = source.getRetriever();
+		System.out.println("Executing SQL...");
+		Connection conn = retriever.getConnection();
+		conn.setAutoCommit(false);
+		Statement st = conn.createStatement();
+		st.setFetchSize(1000);
+
+		ResultSet stopsData = st.executeQuery("SELECT to_fill.stop_id, trim(filled.street) " //
+				+ "FROM stops_moves.taxi_sanfrancisco_stop to_fill inner join "//
+				+ "stops_moves.taxi_sanfrancisco_stop filled on ST_DWithin(ST_Transform(ST_SetSRID(ST_Point(to_fill.centroid_lon,to_fill.centroid_lat), 4326), 3857), "//
+				+ "ST_Transform(ST_SetSRID(ST_Point(filled.centroid_lon,filled.centroid_lat), 4326), 3857), 5) "//
+				+ "where to_fill.stop_id <> filled.stop_id "
+				+ "and to_fill.street is null "
+				+ "and filled.street is not null ");
+		Map<Integer, String> toFill = new HashMap<>();
+		System.out.println("Fetching...");
+		while(stopsData.next()) {
+			toFill.put(stopsData.getInt(1), stopsData.getString(2));
+		}
+		System.out.printf("To update %d registers", toFill.size());
+		stopsData.close();
+		st.close();
+		int registers = 0;
+		conn.setAutoCommit(false);
+		PreparedStatement ps = conn.prepareStatement("update stops_moves.taxi_sanfrancisco_stop set street = ? where stop_id = ?");
+		for (Map.Entry<Integer, String> entry : toFill.entrySet()) {
+			ps.setString(1, entry.getValue());
+			ps.setInt(2, entry.getKey());
+			ps.addBatch();
+			registers++;
+			if(registers % 500 == 0) {
+				ps.executeBatch();
+				conn.commit();
+			}
+		}
+		ps.executeBatch();
+		conn.commit();
+		
+	}
+}
---
+++ artigo/src/test/java/br/ufsc/lehmann/geocode/reverse/SanFranciscoTaxiReverseGeocoding.java
@@ -0,0 +1,109 @@
+package br.ufsc.lehmann.geocode.reverse;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.asynchttpclient.DefaultAsyncHttpClient;
+
+import br.ufsc.core.trajectory.TPoint;
+import br.ufsc.core.trajectory.semantic.Stop;
+import br.ufsc.db.source.DataRetriever;
+import br.ufsc.db.source.DataSource;
+import br.ufsc.db.source.DataSourceType;
+
+public class SanFranciscoTaxiReverseGeocoding {
+
+	public static void main(String[] args) throws SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException {
+		DataSource source = new DataSource("postgres", "postgres", "localhost", 5432, "postgis", DataSourceType.PGSQL, "taxi.sanfrancisco_taxicab_crawdad", null,
+				null);
+		DataRetriever retriever = source.getRetriever();
+		System.out.println("Executing SQL...");
+		Connection conn = retriever.getConnection();
+		conn.setAutoCommit(false);
+		Statement st = conn.createStatement();
+		st.setFetchSize(1000);
+
+		ResultSet stopsData = st.executeQuery("SELECT stop_id, start_lat, start_lon, begin, end_lat, end_lon, length, centroid_lat, " + //
+				"centroid_lon, start_time, end_time, street " + //
+				"FROM stops_moves.taxi_sanfrancisco_stop "//
+				+ "where street is null ");
+		Map<Integer, Stop> stops = new HashMap<>();
+		System.out.println("Fetching...");
+		while (stopsData.next()) {
+			int stopId = stopsData.getInt("stop_id");
+			Stop stop = stops.get(stopId);
+			if (stop == null) {
+				stop = new Stop(stopId, null, //
+						stopsData.getTimestamp("start_time").getTime(), //
+						stopsData.getTimestamp("end_time").getTime(), //
+						new TPoint(stopsData.getDouble("start_lat"), stopsData.getDouble("start_lon")), //
+						stopsData.getInt("begin"), //
+						new TPoint(stopsData.getDouble("end_lat"), stopsData.getDouble("end_lon")), //
+						stopsData.getInt("length"), //
+						new TPoint(stopsData.getDouble("centroid_lat"), stopsData.getDouble("centroid_lon")),//
+						stopsData.getString("street")//
+				);
+				stops.put(stopId, stop);
+			}
+		}
+		System.out.printf("Updating %d stops\n", stops.size());
+		PreparedStatement ps = conn.prepareStatement("update stops_moves.taxi_sanfrancisco_stop set street = ? where stop_id = ?");
+		ReverseGeocoding geocoding = new ReverseGeocoding();
+		int registers = 0, counter = 0;
+		DefaultAsyncHttpClient asyncHttpClient = new DefaultAsyncHttpClient();
+		try {
+//			try {
+//				Thread.sleep(5 * 60 * 1000);
+//			} catch (InterruptedException e1) {
+//				throw new RuntimeException(e1);
+//			}
+			for (Iterator iterator = stops.entrySet().iterator(); iterator.hasNext();) {
+				registers++;
+				Map.Entry<Integer, Stop> entry = (Map.Entry<Integer, Stop>) iterator.next();
+				boolean added = false;
+				while (!added) {
+					try {
+						Place place = geocoding.fromLatLon(asyncHttpClient, entry.getValue().getCentroid().getX(),
+								entry.getValue().getCentroid().getY());
+						ps.setString(1, place.getAddress().getRoad());
+						ps.setInt(2, entry.getKey());
+						ps.addBatch();
+						added = true;
+					} catch (IllegalArgumentException e) {
+						try {
+							Thread.sleep(5 * 60 * 1000);
+							System.err.println("Trying again...");
+						} catch (InterruptedException e1) {
+							throw new RuntimeException(e1);
+						}
+					}
+				}
+				if (registers % 100 == 0) {
+					System.out.printf("%d\n", counter++);
+					ps.executeBatch();
+					conn.commit();
+					try {
+						Thread.sleep(1 * 60 * 1000);
+					} catch (InterruptedException e1) {
+						throw new RuntimeException(e1);
+					}
+				}
+			}
+			ps.executeBatch();
+			conn.commit();
+		} catch (SQLException e) {
+			if (e.getNextException() != null) {
+				throw e.getNextException();
+			}
+			throw e;
+		} finally {
+			asyncHttpClient.close();
+		}
+	}
+}
