26c99e69d9bc9c01a0c79e164e5b9a1b1f82b4ae
André <Andr@Limao>
2018-01-23 12:37:55 -0200

    Backup

--- tcore/src/br/ufsc/core/trajectory/TimestampSemantic.java
+++ tcore/src/br/ufsc/core/trajectory/TimestampSemantic.java
@@ -12,7 +12,9 @@ public class TimestampSemantic extends Semantic<Instant, Number> {
 
 	@Override
 	public boolean match(SemanticTrajectory a, int i, SemanticTrajectory b, int j, Number threshlod) {
-		return distance(a, i, b, j) <= threshlod.doubleValue();
+		TemporalDuration a1 = (TemporalDuration) a.getDimensionData(index, i);
+		TemporalDuration b1 = (TemporalDuration) b.getDimensionData(index, j);
+		return _distance(a1.getStart(), b1.getStart()) <= threshlod.doubleValue();
 	}
 
 	@Override
@@ -24,16 +26,21 @@ public class TimestampSemantic extends Semantic<Instant, Number> {
 
 	@Override
 	public double distance(Instant d1, Instant d2) {
-		long until = d1.until(d2, ChronoUnit.MINUTES);
-		if(until == 0) {
+		double until = _distance(d1, d2);
+		return 1 - (1 / Math.abs(until));
+	}
+
+	private double _distance(Instant d1, Instant d2) {
+		double until = d1.until(d2, ChronoUnit.MINUTES);
+		if(until == 0.0) {
 			return 0.0;
 		}
-		return 1 - (1 / Math.abs(until));
+		return until;
 	}
 
 	@Override
 	public boolean match(Instant d1, Instant d2, Number threshold) {
-		return distance(d1, d2) <= threshold.doubleValue();
+		return _distance(d1, d2) <= threshold.doubleValue();
 	}
 
 }
