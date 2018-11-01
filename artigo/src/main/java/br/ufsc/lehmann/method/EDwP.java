52bd907e41502e8e7d09b0000d856edc0bd67b4c
André <Andr@150.162.208.95>
2018-11-01 09:41:12 -0300

    Making EDwP Thread-safe

--- artigo/src/main/java/br/ufsc/lehmann/method/EDwP.java
+++ artigo/src/main/java/br/ufsc/lehmann/method/EDwP.java
@@ -16,11 +16,8 @@ import br.ufsc.utils.Distance;
 import br.ufsc.utils.EuclideanDistanceFunction;
 
 public class EDwP extends TrajectorySimilarityCalculator<SemanticTrajectory> implements IMeasureDistance<SemanticTrajectory> {
-	double totalLength;
-	public Matrix matrix;
-	double time;
-	boolean computeTime;
 	private SpatialDistanceFunction distFunc;
+	private boolean computeTime;
 
 	public String getName() {
 		return "EDwP";
@@ -58,14 +55,14 @@ public class EDwP extends TrajectorySimilarityCalculator<SemanticTrajectory> imp
 
 		STrajectory t1 = new STrajectory(0, -1, null, Arrays.asList(t1Point));
 		STrajectory t2 = new STrajectory(0, -1, null, Arrays.asList(t2Point));
-		this.matrix = new Matrix(t1.edges.size() + 1, t2.edges.size() + 1);
+		Matrix matrix = new Matrix(t1.edges.size() + 1, t2.edges.size() + 1);
 
-		initializeMatrix();
+		initializeMatrix(matrix);
 
-		this.totalLength = (Distance.getDistance(t1Point, distFunc) + Distance.getDistance(t2Point, distFunc));
+		double totalLength = (Distance.getDistance(t1Point, distFunc) + Distance.getDistance(t2Point, distFunc));
 
-		for (int i = 1; i < this.matrix.numRows(); i++) {
-			for (int j = 1; j < this.matrix.numCols(); j++) {
+		for (int i = 1; i < matrix.numRows(); i++) {
+			for (int j = 1; j < matrix.numCols(); j++) {
 				double rowDelta = Double.MAX_VALUE;
 				double colDelta = Double.MAX_VALUE;
 				double rowCoverage1 = Double.MAX_VALUE;
@@ -79,24 +76,24 @@ public class EDwP extends TrajectorySimilarityCalculator<SemanticTrajectory> imp
 				TPoint t2Edit = null;
 				TPoint t1Edit = null;
 				if (i > 1) {
-					t1Edit = this.matrix.rowEdits[(i - 1)][j];
-					t2Edit = this.matrix.colEdits[(i - 1)][j];
+					t1Edit = matrix.rowEdits[(i - 1)][j];
+					t2Edit = matrix.colEdits[(i - 1)][j];
 					double prevPointEdge = distFunc.distance(t1Edit, t1Point[i - 1]);
 
 					t2Insert = lineMap((TPoint) t2Edit, t2Point[j], t1Point[i - 1]);
 					double rowEditDistance = distFunc.distance(t2Insert, t1Point[i - 1]);
 					double rowEditEdge = distFunc.distance(t2Edit, t2Insert);
 					t2Insert.setTime(0.0D);
-					rowCoverage1 = (rowEditEdge + prevPointEdge) / this.totalLength;
-					rowCoverage2 = (distFunc.distance(t2Point[j], t2Insert) + t1.edgeLength(i - 1)) / this.totalLength;
-					rowDelta = this.matrix.value[(i - 1)][j] - this.matrix.delta[(i - 1)][j]
+					rowCoverage1 = (rowEditEdge + prevPointEdge) / totalLength;
+					rowCoverage2 = (distFunc.distance(t2Point[j], t2Insert) + t1.edgeLength(i - 1)) / totalLength;
+					rowDelta = matrix.value[(i - 1)][j] - matrix.delta[(i - 1)][j]
 							+ (rowEditDistance + distFunc.distance(t1Edit, t2Edit)) * rowCoverage1;
 					rowSpatialScore = rowDelta
 							+ (rowEditDistance + distFunc.distance(t2Point[j], t1Point[i])) * rowCoverage2;
 				}
 				if (j > 1) {
-					t1Edit = this.matrix.rowEdits[i][(j - 1)];
-					t2Edit = this.matrix.colEdits[i][(j - 1)];
+					t1Edit = matrix.rowEdits[i][(j - 1)];
+					t2Edit = matrix.colEdits[i][(j - 1)];
 					if (t1Edit == null) {
 						break;
 					}
@@ -108,34 +105,34 @@ public class EDwP extends TrajectorySimilarityCalculator<SemanticTrajectory> imp
 
 					t1Insert.setTime(0.0D);
 
-					colCoverage1 = (colEditEdge + prevPointEdge) / this.totalLength;
-					colCoverage2 = (distFunc.distance(t1Point[i], t1Insert) + t2.edgeLength(j - 1)) / this.totalLength;
-					colDelta = this.matrix.value[i][(j - 1)] - this.matrix.delta[i][(j - 1)]
+					colCoverage1 = (colEditEdge + prevPointEdge) / totalLength;
+					colCoverage2 = (distFunc.distance(t1Point[i], t1Insert) + t2.edgeLength(j - 1)) / totalLength;
+					colDelta = matrix.value[i][(j - 1)] - matrix.delta[i][(j - 1)]
 							+ (colEditDistance + distFunc.distance(t1Edit, t2Edit)) * colCoverage1;
 					colSpatialScore = colDelta
 							+ (colEditDistance + distFunc.distance(t1Point[i], t2Point[j])) * colCoverage2;
 				}
-				double diagCoverage = (t1.edgeLength(i - 1) + t2.edgeLength(j - 1)) / this.totalLength;
+				double diagCoverage = (t1.edgeLength(i - 1) + t2.edgeLength(j - 1)) / totalLength;
 
 				double subScore = (distFunc.distance(t2Point[j], t1Point[i])
 						+ distFunc.distance(t2Point[j - 1], t1Point[i - 1])) * diagCoverage;
 
-				double diagScore = this.matrix.value[(i - 1)][(j - 1)] + subScore;
+				double diagScore = matrix.value[(i - 1)][(j - 1)] + subScore;
 				if ((diagScore <= colSpatialScore) && (diagScore <= rowSpatialScore)) {
-					this.matrix.add(i, j, diagScore, (byte) 1, t2Point[j - 1], t1Point[i - 1]);
-					this.matrix.delta[i][j] = (diagScore - this.matrix.value[(i - 1)][(j - 1)]);
+					matrix.add(i, j, diagScore, (byte) 1, t2Point[j - 1], t1Point[i - 1]);
+					matrix.delta[i][j] = (diagScore - matrix.value[(i - 1)][(j - 1)]);
 				} else if ((colSpatialScore < rowSpatialScore)
 						|| ((colSpatialScore == rowSpatialScore) && (t2.edges.size() > t1.edges.size()))) {
-					this.matrix.add(i, j, colSpatialScore, (byte) 2, t2Point[j - 1], t1Insert);
-					this.matrix.delta[i][j] = (colSpatialScore - colDelta);
+					matrix.add(i, j, colSpatialScore, (byte) 2, t2Point[j - 1], t1Insert);
+					matrix.delta[i][j] = (colSpatialScore - colDelta);
 				} else {
-					this.matrix.add(i, j, rowSpatialScore, (byte) 3, t2Insert, t1Point[i - 1]);
-					this.matrix.delta[i][j] = (rowSpatialScore - rowDelta);
+					matrix.add(i, j, rowSpatialScore, (byte) 3, t2Insert, t1Point[i - 1]);
+					matrix.delta[i][j] = (rowSpatialScore - rowDelta);
 				}
 			}
 		}
-		// double[] answer = { this.matrix.score(), this.time };
-		return this.matrix.score();
+		// double[] answer = { matrix.score(), this.time };
+		return matrix.score();
 	}
 
 	private TPoint[] getAllPoints(SemanticTrajectory t1) {
@@ -188,18 +185,18 @@ public class EDwP extends TrajectorySimilarityCalculator<SemanticTrajectory> imp
 		return new TPoint(xi, yi, new Timestamp(-1));
 	}
 
-	private void initializeMatrix() {
-		for (int i = 1; i < this.matrix.value.length; i++) {
-			this.matrix.value[i][0] = Double.MAX_VALUE;
+	private void initializeMatrix(Matrix matrix) {
+		for (int i = 1; i < matrix.value.length; i++) {
+			matrix.value[i][0] = Double.MAX_VALUE;
 		}
-		for (int j = 1; j < this.matrix.value[0].length; j++) {
-			this.matrix.value[0][j] = Double.MAX_VALUE;
+		for (int j = 1; j < matrix.value[0].length; j++) {
+			matrix.value[0][j] = Double.MAX_VALUE;
 		}
-		this.matrix.value[0][0] = 0.0D;
+		matrix.value[0][0] = 0.0D;
 	}
 
-	public void printPath() {
-		this.matrix.printPath();
+	public void printPath(Matrix matrix) {
+		matrix.printPath();
 	}
 
 	public TPoint lineMap(TPoint point1, TPoint point2, TPoint point) {
